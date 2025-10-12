2005145e308ef52002b5b4efc253f9346ede074d
Jhonn Gutierrez <jhonngutierrez.contact@gmail.com>
2025-10-12 18:53:12 -0500

    fix: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwindcss(), react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
